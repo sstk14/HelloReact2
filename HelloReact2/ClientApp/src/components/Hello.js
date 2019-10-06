cf36519439ae9a7367bdd844502473bde6c0fc8b
Takashi-PC <jed_420taurus@docomo.ne.jp>
2019-10-06 15:04:59 +0900

    プロジェクト ファイルを追加します。

---
+++ HelloReact2/ClientApp/src/components/Hello.js
@@ -0,0 +1,12 @@
+﻿import React, { Component } from 'react';
+
+export class Hello extends Component {
+
+    render() {
+        return (
+            <div>
+                <h1>Hello, World!</h1>
+            </div>
+        );
+    }
+}
